35df19cff1ead6b1e8d354847d0a4efb92aab2b6
Marcos Parrotto <marcosparrotto@gmail.com>
2022-01-12 21:05:49 -0300

    Finalizando inserçao

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -18,7 +18,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 
 }
 
@@ -32,8 +32,14 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
     .then((response: any) => setTransaction(response.data.transactions))
   }, [])
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transaction', transaction)
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transaction', {...transactionInput, createdAt: new Date() })
+    const {transaction} = response.data as any;
+
+    setTransaction([
+      ...transactions,
+      transaction
+    ]);
   }
 
   return (
